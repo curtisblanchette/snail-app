eac5d56b932c91364246900247eaf8ea579d7ac0
curtisblanchette <curtis.blanchette88@gmail.com>
2019-03-02 00:11:18 -0800

    add login route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SnailFormComponent } from './components/snail/snail.component';
+import { LoginFormComponent } from './components/login/login-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login-form', component: LoginFormComponent },
+  { path: 'snail', component: SnailFormComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
