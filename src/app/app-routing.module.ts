6cdf4a34b4dff4ab3cf1e032a3fcfab4bdaa028b
curtisblanchette <curtis.blanchette88@gmail.com>
2019-03-02 21:38:13 -0800

    update app component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SnailFormComponent } from './components/snail/snail.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'snail', component: SnailFormComponent },
+  { path: 'metrics', loadChildren: './modules/metrics/metrics.module#MetricsModule'},
 ];
 
 @NgModule({
