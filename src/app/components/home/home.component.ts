f3c2cdef7f266a26956dc58fed11f7170368fb4a
curtisblanchette <curtis.blanchette88@gmail.com>
2019-03-03 14:03:45 -0800

    add users login/register forms and service calls

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,21 +1,61 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.scss']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
+  private registrationSuccess = false;
+  private authenticated = false;
+
+  private registerForm: FormGroup = new FormGroup({
+    username: new FormControl('', [
+      Validators.required
+    ]),
+    password: new FormControl('', [
+      Validators.required
+    ])
+  });
+
+  private loginForm: FormGroup = new FormGroup({
+    username: new FormControl('', [
+      Validators.required
+    ]),
+    password: new FormControl('', [
+      Validators.required
+    ])
+  });
 
   constructor(
-
+    private dataService: DataService
   ) {
 
   }
 
   ngOnInit() {
 
+  }
+
+  login() {
+    this.dataService.login({
+      username: this.loginForm.controls.username.value,
+      password: this.loginForm.controls.password.value,
+    }).subscribe((results) => {
+      this.authenticated = true;
+    });
+  }
+
+  register() {
+    this.dataService.register({
+      username: this.registerForm.controls.username.value,
+      password: this.registerForm.controls.password.value,
+    }).subscribe((results) => {
+      this.registrationSuccess = true;
+    });
   }
 
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -13,20 +13,30 @@ const httpOptions = {
 @Injectable()
 export class DataService {
 
+  private baseUrl = 'http://localhost:3000';
+
   constructor(
     private http: HttpClient
   ) { }
 
+  register(data): Observable<any> {
+    return this.http.post<any>(`${this.baseUrl}/users/register`, data, httpOptions);
+  }
+
+  login(data): Observable<any> {
+    return this.http.post<any>(`${this.baseUrl}/users/login`, data, httpOptions);
+  }
+
   solveProblem(data): Observable<any> {
-    return this.http.post<Snail>('http://localhost:3000/results', data, httpOptions);
+    return this.http.post<Snail>(`${this.baseUrl}/results`, data, httpOptions);
   }
 
   getAllRequestLogs(): Observable<any> {
-    return this.http.get<Snail>('http://localhost:3000/logs', httpOptions);
+    return this.http.get<Snail>(`${this.baseUrl}/logs`, httpOptions);
   }
 
   getAverageSuccessRate(): Observable<any> {
-    return this.http.get<Snail>('http://localhost:3000/results/aggregate', httpOptions);
+    return this.http.get<Snail>(`${this.baseUrl}/results/aggregate`, httpOptions);
   }
 
 }
