d8a534d3ef2b756aa056c6ae33b7ba45534a0dba
Curt Blanchette <6607883+curtisblanchette@users.noreply.github.com>
2019-03-03 23:02:47 -0800

    Update one-decimal-place.pipe.ts

--- src/app/pipes/one-decimal-place.pipe.ts
+++ src/app/pipes/one-decimal-place.pipe.ts
@@ -1,6 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
 /*
- * Raise take a decimal/float number and rounds it to 1 decimal place
+ * Tsake a decimal/float number and rounds it to 1 decimal place
  * Usage:
  *   value | oneDecimalPlace
  * Example:
